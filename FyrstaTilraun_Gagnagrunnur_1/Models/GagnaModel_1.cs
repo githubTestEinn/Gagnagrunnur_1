6128fc064d24fbbb494a814b9cf4089f3e0ef7a7
SveinnArnar <sveas@ru.is>
2020-02-10 07:14:14 +0000

    Fyrsta kommitt

---
+++ FyrstaTilraun_Gagnagrunnur_1/Models/GagnaModel_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FyrstaTilraun_Gagnagrunnur_1.Models
+{
+    public class GagnaModel_1
+    {
+        [Key]
+        public int id { get; set; }
+        [Column(TypeName ="nvarchar(10)")]
+        public int uttekt { get; set; }
+        [Column(TypeName = "nvarchar(140)")] // alveg eins og á twitter :)
+        public string athugasemd { get; set; }
+
+    }
+}
---
+++ FyrstaTilraun_Gagnagrunnur_1/Models/Gagnagrunnur_1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FyrstaTilraun_Gagnagrunnur_1.Models
+{
+    public class Gagnagrunnur_1 : DbContext
+    {
+        public Gagnagrunnur_1(DbContextOptions<Gagnagrunnur_1> options):base(options)
+        {
+
+        }
+
+        // https://www.youtube.com/watch?v=NemyDIUcC64&t=1652s
+        // kominn til 6:22 núna (í dag er 9.2.2020, klukkan 22:30)
+    }
+}
